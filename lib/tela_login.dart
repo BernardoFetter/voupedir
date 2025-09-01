01225a0e5132aa5a851175c10cd8a7f1c95b2c65
BernardoFetter <bernardofetter17@gmail.com>
2025-09-01 10:33:17 -0300

    modificando a tela de login,adicionando assests um ebanco de dados

--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -1,17 +1,19 @@
 import 'dart:ui';
+import 'package:voupedir/banco/usuario_dao.dart';
 import 'usuario.dart';
 import 'tela_home.dart';
 import 'package:flutter/material.dart';
+import 'banco/usuario_dao.dart';
 
 class TelaLogin extends StatelessWidget{
   TelaLogin({super.key});
 
-  Usuario u = Usuario(
+/*Usuario u = Usuario(
     nome: 'admin',
     login: 'admin',
     senha: '@senhaforte123',
     codigo: 1
-  );
+  );*/
 
   final TextEditingController usuarioController = TextEditingController();
   final TextEditingController senhaController = TextEditingController();
@@ -35,8 +37,11 @@ class TelaLogin extends StatelessWidget{
                 controller: senhaController,
               ),
               SizedBox(height: 20),
-              ElevatedButton(onPressed: (){
-                if(u.login == usuarioController.text && u.senha == senhaController.text){
+              ElevatedButton(onPressed: () async {
+
+                final sucesso = await UsuarioDAO.autenticar(usuarioController.text, usuarioController.text);
+
+                if(sucesso){
                   Navigator.push(context,
                   MaterialPageRoute(builder: (context) => TelaHome())
                   );
